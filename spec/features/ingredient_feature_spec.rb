0d52accdbb9a33b7ac916990376bf3226b0e8eb4
John Pfingst <jpfingst@outlook.com>
2017-04-25 18:41:58 +0000

    add some more tests

---
+++ spec/features/ingredient_feature_spec.rb
@@ -0,0 +1,31 @@
+describe 'Feature Test: Ingredient', :type => :feature do
+
+  describe 'Ingredient List' do
+    before(:each) do
+      @water = Ingredient.find_by(name: 'Water')
+      visit ingredients_path
+    end
+
+    it 'has a page title' do
+      expect(page).to have_content 'Ingredients'
+    end
+
+    it 'lists water as an ingredient' do
+      expect(page).to have_content @water.name
+    end
+
+    it 'links to detail page' do
+      expect(page).to have_link @water.name
+    end
+  end
+
+  describe 'Ingredient Detail' do
+    it 'has detail page' do
+      @water = Ingredient.find_by(name: 'Water')
+      visit ingredient_path @water
+      expect(page).to have_content @water.name
+      expect(page).to have_css('h3', :text => @water.name)
+      expect(page).to have_xpath("//img[contains(@src, 'water')]")
+    end
+  end
+end
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,7 +3,6 @@ require 'support/helpers/session_helpers'
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.include ControllerHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Features::SessionHelpers, type: :feature
 end
