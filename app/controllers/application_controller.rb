1ac1cf683aa23057f0b39fe9d923c6e261cb82b3
John Pfingst <jpfingst@outlook.com>
2017-04-25 15:26:57 +0000

    fix up about and welcome pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:welcome]
+  skip_before_action :authenticate_user!, only: [:about, :welcome]
 
   def about
   end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,4 @@
 class RecipesController < ApplicationController
-
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
