d29cf06714f736055d1eab0e7c18a98bdd741d5c
John Pfingst <jpfingst@outlook.com>
2017-04-26 23:58:58 +0000

    missed pundit on Ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,6 @@
 class IngredientsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!,   only: [:index, :show]
+  skip_after_action  :verify_policy_scoped, only: [:index, :show]
 
   def index
     @ingredients = Ingredient.order(:name)
