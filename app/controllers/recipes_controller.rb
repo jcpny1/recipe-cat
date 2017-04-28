a0e809ceeabbf474e6847dccc0a48f3628d49445
John Pfingst <jpfingst@outlook.com>
2017-04-28 10:39:38 -0400

    fixed Favorites bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find_by(id: params[:id])
     authorize @recipe
-    @user_recipe_favorite = UserRecipeFavorite.find_or_initialize_by(user_id: @recipe.user_id, recipe_id: @recipe.id)
+    @user_recipe_favorite = UserRecipeFavorite.find_or_initialize_by(user_id: current_user.id, recipe_id: @recipe.id)
   end
 
   def new
