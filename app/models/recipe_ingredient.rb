91b660728d85357347432af16c4c41df05b35dee
John Pfingst <jpfingst@outlook.com>
2017-04-28 13:39:56 -0400

    corrected location of <tr> for linking.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,4 +2,6 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
   belongs_to :unit
+
+  scope :ingredient_id, -> (ingredient_id) { where ingredient_id: ingredient_id }
 end
