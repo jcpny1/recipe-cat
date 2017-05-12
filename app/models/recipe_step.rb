5e28d2a79dd003a50fcfe362b447f802aa585ba7
John Pfingst <jpfingst@outlook.com>
2017-05-12 17:23:48 -0400

    fix step renumbering error

--- app/models/recipe_step.rb
+++ app/models/recipe_step.rb
@@ -8,7 +8,7 @@ class RecipeStep < ApplicationRecord
 
   def self.renumber(recipe_steps)
     step_counter = 1
-    recipe_steps.each { |recipe_step|
+    recipe_steps.sort { |rs1, rs2| rs1.step_number <=> rs2.step_number }.each { |recipe_step|
       recipe_step.update(step_number: step_counter) if recipe_step != step_counter
       step_counter += 1
     }
