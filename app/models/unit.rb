52e635df1fb5df5f24a3cc7cdd52ab7b7e881ee0
John Pfingst <jpfingst@outlook.com>
2017-07-18 23:54:13 -0400

    Display unit name, not id. Show/Hide Recipe Ingredients toggle.

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,8 +3,8 @@ class Unit < ApplicationRecord
   has_many :unit_conversions
   has_many :from_units, class_name: "UnitConversion", foreign_key: "from_unit_id"
   has_many :to_units,   class_name: "UnitConversion", foreign_key: "to_unit_id"
+  has_many :recipe_ingredients
   validates :name, uniqueness: true
-
 
   # returns an ordered list of units to use in a select list.
   def self.pick_list
--- app/serializers/recipe_ingredient_serializer.rb
+++ app/serializers/recipe_ingredient_serializer.rb
@@ -1,4 +1,5 @@
 class RecipeIngredientSerializer < ActiveModel::Serializer
   attributes :id, :quantity, :unit_id
   has_one :ingredient
+  has_one :unit
 end
