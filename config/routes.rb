f5d827a9bdf30b64507cf9418db418e27bb7a966
John Pfingst <jpfingst@outlook.com>
2017-07-19 14:49:24 -0400

    Pull down devise login form to edit for Bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations', sessions: 'users/sessions' }
 
   resources :ingredients, only: [:index, :show]
 
