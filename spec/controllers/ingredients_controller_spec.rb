c057a01b91666d6857cb887461033025a0da2de9
John Pfingst <jpfingst@outlook.com>
2017-12-23 00:23:00 -0500

    Add some ingredient controller tests.

--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe IngredientsController, type: :controller do
+  before do
+    ingredient_attributes = {name: "Test Ingredient", photo_path: "egg.png", description: "a egg"}
+    @ingredient = Ingredient.create(ingredient_attributes)
+  end
+
+  describe "GET #index" do
+    it "returns all ingredients" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it 'returns an ingredient' do
+      get :show, params: { id: @ingredient.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
